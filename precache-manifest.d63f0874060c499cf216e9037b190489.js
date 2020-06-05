e975830cd15977b067587e9b47bea87e0b124fab
Ayush Majumder <majumdershuvu@gmail.com>
2020-06-05 19:26:55 +0530

    Updates

---
+++ precache-manifest.d63f0874060c499cf216e9037b190489.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "87780c3b45191af216a3",
+    "url": "/MyReads-A-Book-Lending-App/static/css/main.84e62050.chunk.css"
+  },
+  {
+    "revision": "87780c3b45191af216a3",
+    "url": "/MyReads-A-Book-Lending-App/static/js/main.87780c3b.chunk.js"
+  },
+  {
+    "revision": "dfa30fcfa065932bd0e6",
+    "url": "/MyReads-A-Book-Lending-App/static/js/1.dfa30fcf.chunk.js"
+  },
+  {
+    "revision": "b3651617cbcb5f12c690",
+    "url": "/MyReads-A-Book-Lending-App/static/js/runtime~main.b3651617.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/MyReads-A-Book-Lending-App/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/MyReads-A-Book-Lending-App/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/MyReads-A-Book-Lending-App/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "9b6c8aa8d7ce508c8283413d756e0f8c",
+    "url": "/MyReads-A-Book-Lending-App/index.html"
+  }
+];
